23805e9aa7b0b5a0e31f8d6bf77db4bc8e71459c
Lelo Dumeko <Lelozwi@nawirigroup.com>
2025-11-12 22:51:39 +0200

    Gather Lt Gen Mkhwanazi Allegations

--- README.md
+++ README.md
@@ -17,9 +17,26 @@ If you are developing a production application, we recommend using TypeScript wi
 
 ### TO DO
 2. Gather Mkhwanazi allegations 
-3. Group Ad Hoc Committee timeline by months (from formation to where we are now)
-4. Group Testimony by individual and link them to specific parts of Mkhwanazi's allegations and how it affects/includes/implicates them with summary of what they testified in parliament
-5. Why is there a parliamentary ad hoc committee in the 1st place?
-6. Progress on Madlanga commission
-7. Progress on Ad hoc Committee deadline
+3. Create Key Players Page
+    IMC (created in 2018), given a task by the President to ensure that they deal with matters of security nature that affect political leadership in this province, consisted of 
+    - Minister of Police  
+    - Minister of State Security 
+    - Minister of Defence 
+    - Minister of Justice and Constitutional Development
+    - Minister of Correctional Services (used to be merged with Minister of Justice)
+
+    Brown Mogotsi
+    5 March 2025: 
+    Then Minister of Police, Senzo Mchunu, denies on record any knowledge of Brown Mogotsi during the Portfolio Committee on Police.
+        "We must be very circumspect when people come to us and say 'I am an associate of this and that'. I am no associate to the person that General Mkhwanazi is refering to. But I appreciate whatever they discussed and all those things coming out so that I understand who is saying what, where, and so on."
+    
+    21 October 2025:
+    Senzo Mchunu admits knowing ANC-aligned businessman Brown Mogotsi since 2017 but maintains their relationship was strictly casual. He adds that that he has met Vusimuzi "Cat" Matlala (alleged criminal and member of this crime syndicate) but only in the presence of Mr Mogotsi.
+                    
+                   
+4. Group Ad Hoc Committee timeline by months (from formation to where we are now)
+5. Group Testimony by individual and link them to specific parts of Mkhwanazi's allegations and how it affects/includes/implicates them with summary of what they testified in parliament
+6. Why is there a parliamentary ad hoc committee in the 1st place?
+7. Progress on Madlanga commission
+8. Progress on Ad hoc Committee deadline
 
--- src/pages/Allegations.jsx
+++ src/pages/Allegations.jsx
@@ -6,19 +6,116 @@ export function Allegations() {
         <div className="p-8">
              
             <div className="flex items-center mb-6 justify-center">
-                <h1 className="text-2xl mb-4 ">Allegations made by Lt-Gen Mkhwanazi on 6 July 2025</h1>
+                <h1 className=" mb-4 ">Allegations made by Lieutenant General Mkhwanazi on 6 July 2025</h1>
             </div>
            
-            <p className="mb-6 uppercase text-lg text-pretty">A sophisticated criminal syndicate has infiltrated law enforcement and intelligence structures in South Africa</p>
+            <p className="mb-6 uppercase text-2xl text-pretty">A sophisticated criminal syndicate has infiltrated law enforcement and intelligence structures in South Africa</p>
             <div className="flex flex-col gap-4">
                 <h2 className="underline">1. The Political Killings Task Team Disbandment</h2>
-                <p> The Minister of Police allegedly interfered with sensitive police investigations and colluded with business people, including a murder accused, to disband the Political Killings Task Team based in KwaZulu-Natal.</p>
+                <p className="italic"> The Minister of Police allegedly interfered with sensitive police investigations and colluded with business people, including a murder accused, to disband the Political Killings Task Team based in KwaZulu-Natal.  Lt-Gen Mkhwanazi believes that the PKTT assisting Gauteng Crime Intelligence's investigation of a criminal syndicate led to its disbandment.
+                   <br></br> </p>
                 
+                <ul className="ml-4"> 
+                <li>
+                    <span className="font-bold">
+                         2024:
+                    </span>{" "}
+                    Lieutenant General Mkhwanazi deploys 10 members of the PKTT to assist the Organised Crime Investigating Unit based in Gauteng in invetigating the murder of Q Tech Engineering Company employee, Mr A Swart. Q Tech is a long-standing service provider to Transnet. The Organised Crime Unit was investigating the involvement of a syndicate controlled in Gauteng and operating in the rest of the country and neighbouring countries in Mr Swart's murder.
+
+                    <br></br>
+                    {/*
+                   Those discovered to have been involved in this syndicate include:
+                   <ul>
+                    <li>
+                        Current serving members of parliament
+                    </li>
+                    <li>
+                        Members of the South African Police Service, the Metro Police, and Correctional Services
+                    </li>
+                    <li>
+                        Gauteng provincial prosecutors and members of the Judiciary
+                    </li>
+                    <li>
+                        Business people
+                    </li>
+                   </ul> */}
+                </li>
+                <li>
+                    <span className="font-bold">6 December 2024: </span> Mr Katiso "KT" Molefe is arrested alongside a detective within the SAPS for the murder of Mr Swart.  A search and seizure is conducted at Mr Vusimuzi "Cat" Matlala's home as well. Weapons are confiscated and sent for ballistics testing by the PKTT.
+                </li>
+
+                {/* <li>
+                    <span className="font-bold">17 December 2024: </span> Mr Vusimuzi Matlala allegedly files a false theft complaint against the PKTT under the instruction of Lieutenant General Sibiya.
+                </li>
+
+                  <li>
+                    <span className="font-bold">18 - 19 December 2024: </span> PKTT visits Mr Matlala's offices to conduct a compliance inspection of his firearms.
+                </li> */}
+
+                <li>
+                    <span className="font-bold">30 December 2024: </span> A ballistics report links the firearms not only with the murder of Mr. Swart, but also high-profile cases involving South African artists as far back as 2021.
+                </li>
+                <li>
+                    <span className="font-bold">31 December 2024: </span> Minister of Police, Mr Senzo Mchunu, issues a letter to disband the political killings task team and suspend the filling of vacancies within Crime Intelligence. 
+                </li>
+                <li>
+                    <span className="font-bold">26 March 2025:</span> As a result of the order of disbandment, the Deputy National Comissioner of Crime Detection, Lieutenant General Sibiya, instructs 121 case dockets be taken from the task team to his office. Since then, no further investigation work was done on those dockets. 5 of which, Gen Mkhwanazi claims, contained instructions to arrest perpetrators.
+                </li> 
+                 <li>
+                     <span className="font-bold">14 May 2025: </span> The PKTT arrests Mr Vusimuzi Matlala for attempted murder. Analysis of his electronic devices revealed communication between Mr Matlala, Mr Mogotsi and the Minister of Police, Mr Mchunu. On January 1 2025, Mr Mogotsi texts a WhatsApp message to Mr Matlala which reads: "I have arranged a meeting for Sibiya and the Minister. They must have a solution." That is followed by: "The task team that came to your house and harassed you have been dissolved. They got the letter on Monday. Today, as we speak, they are bringing dockets to Sibiya."
+                </li>
+                </ul>
                 <h2 className="underline">2. Existence and operation of a sophisticated crimimal syndicate involving public servants and politicians.  </h2>
-                <p>A police investigation by the task team in Gauteng unmasked a syndicate controlled by a drug cartel, which involves politicians, law enforcement officials from the SAPS, metro police and correctional services, prosecutors and the judiciary, as well as business people.</p>
+                <ul>
+                    <li>
+                        <p className="italic">
+                        Sensitive information is being leaked to private individuals who are members of this syndicate, enabling them to evade arrest and prosecution. Thus, undermining our country's criminal justice system. 
+                        </p>
+                        <br />
+                       
+                         <ul>
+                        <li>
+                            Mr Brown Mogotsi sent a copy of the letter disbanding the PKTT to Mr Vusimuzi Matlala on 2 January 2025.
+                        </li>
+                        <li>
+                            Mr Matlala sends a copy of the invoice he submitted to the SAPS to Mr Mogotsi.
+                        </li>
+                        <li>
+                            WhatsApp messages from Mr Mogotsi to Lieutenant General Mkhwanazi include the following:
+                            <ul>
+                                <li>
+                                    Knowledge of a case involving the Departmet of Correctional Services facilities located in Empangeni, KwaZulu-Natal long before the incident was publicised.
+                                </li>
+                                <li>
+                                Copies of police letters, occurence books and a register that is only to be accessed by police officers.
+                                </li>
+                                <li>
+                                A screenshot of the presentation of 2024/2025 Division Crime Intelligence Policies.
+                                </li>
+                                <li>
+                                    Knowledge that the Minister of Police had just arrived in Lusikisiki following the mass murder incident that occured there earlier this year.
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    
+                    {/* The Chief of Staff of the Minister of Police, Mr Nkabinde, allegedly confirmed that a Mr Brown Mogotsi was an associate of the Minister of Police but was not formally employed in a department. */} 
+            
+                    </li>
+                    <br></br>
+                    <li>
+                        <p className="italic">Political interference and collusion of public officials in organised crime investigations.</p>
+                        <br />
+                        <div>
+                            A member (or members) of Crime Intelligence leaked classified documents (including vetting documents and the locations of secret properties) to a member of parliament (MP). That same MP opened a criminal case in Gauteng alleging that some Crime Intelligence officials had received fraudulent vetting clearance certificates. Aftwerward, the MP wrote to the Minister of Police, asking him to assign this investigation into Crime Intelligence's vetting process elsewhere. The Minister's Chief of Staff, Mr Nkabinde, sent a letter to the head of the newly formed Investigating Directorate Against Corruption (IDAC), directing them to investigate the case. IDAC took on the dockets and began investigating, which raises questions about whether such cases fall within IDAC's intended mandate.
+                        </div> 
+                    </li>
+                </ul>           
             </div>
+
+           <a target="blank" href="https://www.politicsweb.co.za/documents/transcript-nhlanhla-mkhwanazis-bombshell-press-con?utm_source=chatgpt.com"> A link to the Genral's full media briefing transcript.</a>
         </div>
-        </>
+    </>
         
     );
 }

4938d6fb0b7d90ad704efe06b7e0d00e11ef4b47
Lelo Dumeko <Lelozwi@nawirigroup.com>
2026-02-11 13:51:04 +0200

    Install ReactQuery

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import { Testimony } from './pages/Testimony.jsx';
 import { Allegations } from './pages/Allegations.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
- 
-  return (
-   <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/testimony' element={<Testimony/>}/>
-    <Route path='/allegations' element={<Allegations/>}/>
-   </Routes>
+  const queryClient = new QueryClient();
 
+  return (
+  <QueryClientProvider client={queryClient}>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/testimony' element={<Testimony/>}/>
+      <Route path='/allegations' element={<Allegations/>}/>
+    </Routes>
+  </QueryClientProvider>
   )
 }
 

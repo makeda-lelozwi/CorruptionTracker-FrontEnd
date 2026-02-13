4c4012e9f6a23924c01da68964d2fe69779f2329
Lelo Dumeko <Lelozwi@nawirigroup.com>
2026-02-13 18:13:41 +0200

    Finalise 6 July Allegations

--- src/pages/Allegations.jsx
+++ src/pages/Allegations.jsx
@@ -3,20 +3,24 @@ export function Allegations() {
     return (
         <>
         <Link to="/">Back to Home Page</Link>
-        <div className="p-8">
-             
-            <div className="flex flex-col items-center mb-6 justify-center">
-                <h1 className=" mb-4 ">Allegations made by Lieutenant General Mkhwanazi on 6 July 2025</h1>
-                <a className=" font-bold text-amber-300" target="blank" href="https://www.politicsweb.co.za/documents/transcript-nhlanhla-mkhwanazis-bombshell-press-con?utm_source=chatgpt.com"> The General's full media briefing transcript</a>
+        <div className="flex flex-col justify-center items-center p-8">
+            <div className="flex flex-col items-center mb-6 justify-center px-12 ">
+              <div className="w-full text-center">
+                <h1 className="mb-6 uppercase text-3xl font-bold text-pretty leading-tight max-w-4xl mx-auto">A sophisticated criminal syndicate has infiltrated law enforcement and intelligence structures in South Africa</h1>
+              </div>
+                <p className=" mb-4 ">A summary of the allegations made by Lieutenant General Mkhwanazi on 6 July 2025</p>
+                <a className=" font-bold text-amber-300" target="blank" href="https://www.politicsweb.co.za/documents/transcript-nhlanhla-mkhwanazis-bombshell-press-con?utm_source=chatgpt.com"> You can read the General's full media briefing transcript here.</a>
             </div>
            
-            <p className="mb-6 uppercase text-2xl text-pretty">A sophisticated criminal syndicate has infiltrated law enforcement and intelligence structures in South Africa</p>
-            <div className="flex flex-col gap-4">
-                <h2 className="underline">1. The Political Killings Task Team Disbandment</h2>
-                <p className="italic"> The Minister of Police allegedly interfered with sensitive police investigations and colluded with business people, including a murder accused, to disband the Political Killings Task Team based in KwaZulu-Natal.  Lt-Gen Mkhwanazi believes that the PKTT assisting Gauteng Crime Intelligence's investigation of a criminal syndicate led to its disbandment.
-                   <br></br> </p>
-                
-                <ul className="ml-4"> 
+            <div className="flex flex-col gap-4 max-w-4xl mx-auto px-4">
+              <h2 className="underline font-bold">Allegation #1:</h2>
+              <p className="italic"> 
+                The Minister of Police allegedly interfered with sensitive police investigations when he disbanded the Political Killings Task Team (PKTT) based in KwaZulu-Natal.  
+                Lt-Gen Mkhwanazi believes that this decision was informed by the PKTT's success in assisting Gauteng Crime Intelligence's investigation into a criminal syndicate operating in Gauteng and had nothing to do with the PKTT's work on political killings in KwaZulu Natal.
+                <br></br> 
+              </p>
+              <ul className="ml-4"> 
+                <p className=" font-bold mb-2">Timeline of events leading up to the disbandment of the PKTT:</p>
                 <li>
                     <span className="font-bold">
                          2024:
@@ -24,34 +28,10 @@ export function Allegations() {
                     Lieutenant General Mkhwanazi deploys 10 members of the PKTT to assist the Organised Crime Investigating Unit based in Gauteng in invetigating the murder of Q Tech Engineering Company employee, Mr A Swart. Q Tech is a long-standing service provider to Transnet. The Organised Crime Unit was investigating the involvement of a syndicate controlled in Gauteng and operating in the rest of the country and neighbouring countries in Mr Swart's murder.
 
                     <br></br>
-                    {/*
-                   Those discovered to have been involved in this syndicate include:
-                   <ul>
-                    <li>
-                        Current serving members of parliament
-                    </li>
-                    <li>
-                        Members of the South African Police Service, the Metro Police, and Correctional Services
-                    </li>
-                    <li>
-                        Gauteng provincial prosecutors and members of the Judiciary
-                    </li>
-                    <li>
-                        Business people
-                    </li>
-                   </ul> */}
                 </li>
                 <li>
                     <span className="font-bold">6 December 2024: </span> Mr Katiso "KT" Molefe is arrested alongside a detective within the SAPS for the murder of Mr Swart.  A search and seizure is conducted at Mr Vusimuzi "Cat" Matlala's home as well. Weapons are confiscated and sent for ballistics testing by the PKTT.
                 </li>
-
-                {/* <li>
-                    <span className="font-bold">17 December 2024: </span> Mr Vusimuzi Matlala allegedly files a false theft complaint against the PKTT under the instruction of Lieutenant General Sibiya.
-                </li>
-
-                  <li>
-                    <span className="font-bold">18 - 19 December 2024: </span> PKTT visits Mr Matlala's offices to conduct a compliance inspection of his firearms.
-                </li> */}
 
                 <li>
                     <span className="font-bold">30 December 2024: </span> A ballistics report links the firearms not only with the murder of Mr. Swart, but also high-profile cases involving South African artists as far back as 2021.
@@ -66,11 +46,11 @@ export function Allegations() {
                      <span className="font-bold">14 May 2025: </span> The PKTT arrests Mr Vusimuzi Matlala for attempted murder. Analysis of his electronic devices revealed communication between Mr Matlala, Mr Mogotsi and the Minister of Police, Mr Mchunu. On January 1 2025, Mr Mogotsi texts a WhatsApp message to Mr Matlala which reads: "I have arranged a meeting for Sibiya and the Minister. They must have a solution." That is followed by: "The task team that came to your house and harassed you have been dissolved. They got the letter on Monday. Today, as we speak, they are bringing dockets to Sibiya."
                 </li>
                 </ul>
-                <h2 className="underline">2. Existence and operation of a sophisticated crimimal syndicate involving public servants and politicians.  </h2>
+                <h2 className="underline font-bold">Allegation #2:</h2>
                 <ul>
                     <li>
                         <p className="italic">
-                        Sensitive information is being leaked to private individuals who are members of this syndicate, enabling them to evade arrest and prosecution. Thus, undermining our country's criminal justice system. 
+                        Sensitive police information is being leaked to a sophisticated criminal syndicate. This syndicate is allegedly using this information to evade arrest and prosecution. Thus, undermining our country's criminal justice system. 
                         </p>
                         <br />
                        
@@ -83,35 +63,45 @@ export function Allegations() {
                         </li>
                         <li>
                             WhatsApp messages from Mr Mogotsi to Lieutenant General Mkhwanazi include the following:
-                            <ul>
+                            <ul className="ml-3 flex flex-col gap-1">
                                 <li>
-                                    Knowledge of a case involving the Departmet of Correctional Services facilities located in Empangeni, KwaZulu-Natal long before the incident was publicised.
+                                   <span>a. </span>Knowledge of a case involving the Departmet of Correctional Services facilities located in Empangeni, KwaZulu-Natal long before the incident was publicised.
                                 </li>
                                 <li>
-                                Copies of police letters, occurence books and a register that is only to be accessed by police officers.
+                                <span>b. </span>Copies of police letters, occurence books and a register that is only to be accessed by police officers.
                                 </li>
                                 <li>
-                                A screenshot of the presentation of 2024/2025 Division Crime Intelligence Policies.
+                                <span>c. </span>A screenshot of the presentation of 2024/2025 Division Crime Intelligence Policies.
                                 </li>
                                 <li>
-                                    Knowledge that the Minister of Police had just arrived in Lusikisiki following the mass murder incident that occured there earlier this year.
+                                    <span>d. </span>Knowledge that the Minister of Police had just arrived in Lusikisiki following the mass murder incident that occured there earlier this year.
                                 </li>
                             </ul>
                         </li>
                     </ul>
-                    
-                    {/* The Chief of Staff of the Minister of Police, Mr Nkabinde, allegedly confirmed that a Mr Brown Mogotsi was an associate of the Minister of Police but was not formally employed in a department. */} 
-            
-                    </li>
-                    <br></br>
+                  </li>
+                </ul>    
+                <h2 className="underline font-bold">Allegation #3:</h2>
+                <ul> 
                     <li>
-                        <p className="italic">Political interference and collusion of public officials in organised crime investigations.</p>
+                        <p className="italic">Public officials are interfering in organised crime investigations.</p>
                         <br />
                         <div>
                             A member (or members) of Crime Intelligence leaked classified documents (including vetting documents and the locations of secret properties) to a member of parliament (MP). That same MP opened a criminal case in Gauteng alleging that some Crime Intelligence officials had received fraudulent vetting clearance certificates. Aftwerward, the MP wrote to the Minister of Police, asking him to assign this investigation into Crime Intelligence's vetting process elsewhere. The Minister's Chief of Staff, Mr Nkabinde, sent a letter to the head of the newly formed Investigating Directorate Against Corruption (IDAC), directing them to investigate the case. IDAC took on the dockets and began investigating, which raises questions about whether such cases fall within IDAC's intended mandate.
-                        </div> 
+                        </div>
+                        </li> 
+                    </ul>
+                <h2 className="underline font-bold">Allegation #4:</h2>
+                <ul>
+                    <li>
+                        <p className="italic">
+                            The minister's decision to freeze hiring within Crime Intelligence and the court-ordered ban preventing charged Crime Intelligence officials from entering their work premises have effectively crippled South African Police Service Crime Intelligence at both provincial (KwaZulu-Natal) and national levels.</p>
+                        <br />
+                        <div>
+                          Key Crime Intelligence leadership positions remain unfilled. Lieutenant General Mkhwanazi argues that the judiciary overstepped its authority as the decision should fall under the employer, not the courts. In the absence of the charged Crime Intelligence officials, Brain Padayachee, a former Crime Intelligence official who is now working for IDAC, was appointed lead investigator despite lacking investigative credentials. 
+                        </div>
                     </li>
-                </ul>           
+                </ul>                  
             </div>
 
         </div>
